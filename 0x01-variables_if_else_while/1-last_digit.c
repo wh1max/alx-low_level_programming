a93f4c4b9788dd088ec3d74032f36acb78616543
wh1max <oussama.zino56@gmail.com>
2023-03-20 10:53:42 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	int n;
 	int last_digit;
 	char string[] = "Last digit";
-	srand(time (0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	printf("%s of %d is %d and is ", string, n, last_digit);
