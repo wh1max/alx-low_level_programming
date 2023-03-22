fe5166589c12cf845f44c8edc1d3a78931c2645f
wh1max <oussama.zino56@gmail.com>
2023-03-22 12:20:53 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-nclude <unistd.h>
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,7 +7,7 @@ nclude <unistd.h>
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int main(char c)
 {
 	return (write(1, &c, 1));
 }
