e0cf208a46cd82f61bae66516807cf320abdeb0c
wh1max <oussama.zino56@gmail.com>
2023-03-27 14:46:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
+
 /**
  * main - a simple program that outputs 0-9 separated by commas
  *
  * Return: 0 on success
  */
-int main() {
-  int i, j;
+int main(void)
+{
+	int i;
+	int j;
 
-  for (i = 0; i < 9; i++) {
-    for (j = i + 1; j < 10; j++) {
-      putchar(i + '0');
-      putchar(j + '0');
-      if (i < 8) {
-        putchar(',');
-        putchar(' ');
-      }
-    }
-  }
-
-  putchar('\n');
-  return 0;
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 
