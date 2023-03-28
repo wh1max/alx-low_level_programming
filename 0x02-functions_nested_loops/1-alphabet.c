2b9cffb69fec68b0983fdff40c938461a998ac86
wh1max <oussama.zino56@gmail.com>
2023-03-27 23:26:49 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main
+  *Description: Prints the alphabet in lowercase followed by a new line
+ *
+ *Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
