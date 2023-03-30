c159387c495f3a478d7381a1ef38dea084efa098
wh1max <oussama.zino56@gmail.com>
2023-03-30 04:20:26 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,25 +5,41 @@
   */
 int main(void)
 {
-	unsigned int a = 1, b = 2, c;
-	int i = 2
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
 
-	for (;i < 98; i++)
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
 	{
-		c = a + b;
-		a = b;
-		b = c;
-		printf("%lu", c);
-
-		if (i != 97)
-		{
-			printf(", ");
-		}
-
-		else
-		{
-			printf("\n");
-		}
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
 	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
 	return (0);
 }
