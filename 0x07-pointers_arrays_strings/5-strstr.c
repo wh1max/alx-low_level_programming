bcdb3d6d53860b014b4c7142bfbd9e9f757a18a4
wh1max <oussama.zino56@gmail.com>
2023-04-04 15:21:43 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,6 +11,7 @@
 int _strncmp(char *s1, char *s2, unsigned int b)
 {
 	unsigned int i;
+
 	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
 	{
 		s1++;
@@ -29,6 +30,7 @@ char *_strstr(char *haystack, char *needle)
 	unsigned int len;
 	/*Get length of needle for strncmp*/
 	len = 0;
+
 	while (needle[len] != '\0')
 		len++;
 	/*compare substring*/
