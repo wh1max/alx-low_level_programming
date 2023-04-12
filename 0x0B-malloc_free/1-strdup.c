dd0de89255b5c6cf1807caa3a16f714cb09de76f
wh1max <oussama.zino56@gmail.com>
2023-04-12 16:22:08 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,28 +8,23 @@
  */
 char *_strdup(char *str)
 {
-	int n = 0;
-	int i;
-	char *dup_str = (char *) malloc((n + 1) * sizeof(char));
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
 
 	if (str == NULL)
+		return (NULL);
+	for (n = 0; str[n] != '\0'; n++)
+		;
+	strnew = (char *)malloc(n + 1 * sizeof(char));
+	if (strnew != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			strnew[i] = str[i];
+	} else
 	{
 		return (NULL);
 	}
-	while (str[n] != '\0')
-	{
-		n++;
-	}
-	if (dup_str != NULL)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			dup_str[1] = str[i];
-		}
-	}
-	else
-	{
-		return (NULL);
-	}
-	return (dup_str);
+	strnew[i] = '\0';
+	return (strnew);
 }
