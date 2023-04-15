53e524b8aafdefff178aaaec1f786c3eeb63c636
wh1max <oussama.zino56@gmail.com>
2023-04-15 06:38:20 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MIAN_H
+#include <stdlib.h>
+#include <stdio.h>
 
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
