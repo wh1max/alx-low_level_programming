e78ec97e01a5c5a8afc12a46f6494a8f1547a986
wh1max <oussama.zino56@gmail.com>
2023-04-15 05:55:31 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *malloc_checked - Allocates memory using malloc and quit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+	m = malloc(b);
+
+	if (m == 0)
+	{
+		exit(98);
+	}
+	return (m);
+}
