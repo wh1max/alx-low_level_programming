a7afad7a4038f21a02933bba4057a41ebfa34d96
wh1max <oussama.zino56@gmail.com>
2023-04-17 15:17:08 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 /**
  * struct dog - initializes a variable of type struct dog.
  * @name: name of the dog.
@@ -14,5 +15,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
