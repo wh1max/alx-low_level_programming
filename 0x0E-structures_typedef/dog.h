3b1727333429fd2ccc4e33fb1443b55c37d827b7
wh1max <oussama.zino56@gmail.com>
2023-04-17 14:36:27 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * struct dog - initializes a variable of type struct dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: no return.
+ */
 struct dog
 {
 	char *name;
