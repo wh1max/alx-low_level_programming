b7d896e4a0016b1d67e1f14381591452623ec5ed
wh1max <oussama.zino56@gmail.com>
2023-04-17 16:57:48 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+
+/**
+ * dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+
 /**
  * struct dog - initializes a variable of type struct dog.
  * @name: name of the dog.
@@ -17,5 +23,6 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
