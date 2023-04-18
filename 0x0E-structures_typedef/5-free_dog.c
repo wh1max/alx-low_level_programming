fc163115ceebb3ab6107a6470d7461ea7a8b58e5
wh1max <oussama.zino56@gmail.com>
2023-04-17 17:33:22 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
+ * Return: no return.
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
